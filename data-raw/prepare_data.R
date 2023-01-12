4ddea2cc9f73e1f6ceaa1307e3bf900e087689df
Travis Hinkelman <thinkelman@esassoc.com>
2023-01-11 19:06:30 -0800

    Update pipes and packages in prepare_data.R

--- data-raw/prepare_data.R
+++ data-raw/prepare_data.R
@@ -1,13 +1,13 @@
-library(tidyverse)
+library(dplyr)
 
 # data in data-raw/WaterYearType.csv comes from http://cdec.water.ca.gov/cgi-progs/iodir/WSIHIST
 # needs to be manually updated annually
 
-water_year_type <- read.csv("data-raw/WaterYearType.csv") %>%
-  gather(key = "Valley", value = "WYT", SAC:SJR) %>%
+water_year_type <- read.csv("data-raw/WaterYearType.csv") |>
+  tidyr::gather(key = "Valley", value = "WYT", SAC:SJR) |>
   left_join(tibble(WYT = c("W", "AN", "BN", "D", "C"),
-                   WaterYearType = c("Wet", "Above Normal", "Below Normal", "Dry", "Critical"))) %>%
+                   WaterYearType = c("Wet", "Above Normal", "Below Normal", "Dry", "Critical"))) |>
   mutate(WYT = factor(WYT, levels = c("C", "D", "BN", "AN", "W")),
-                      WaterYearType = factor(WaterYearType, levels = c("Critical", "Dry", "Below Normal", "Above Normal", "Wet")))
+         WaterYearType = factor(WaterYearType, levels = c("Critical", "Dry", "Below Normal", "Above Normal", "Wet")))
 usethis::use_data(water_year_type, overwrite = TRUE)
 
